5ea05da9648c7d2c713c630361600f5223ec19fc
DebDoDab <debbius3@gmail.com>
2020-01-29 00:26:33 +0300

    Added 404 shortcut

--- Homework/views.py
+++ Homework/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from emoji import emojize
 from .models import Group, User, Deadline
@@ -13,6 +13,7 @@ def index(request):
 
 
 def deadlineDetails(request, deadlineId):
+    # TODO template
     deadline = Deadline.objects.get(id=deadlineId)
     return HttpResponse(f"You are currently looking at {deadlineId} deadline. \n"
                         f"{deadline}\n"
@@ -20,21 +21,19 @@ def deadlineDetails(request, deadlineId):
 
 
 def groupDetails(request, groupId):
-    try:
-        group = Group.objects.get(id=groupId)
-        userList = ', '.join(str(user) for user in group.user_set.all())
-        deadlineList = ', '.join(str(deadline) for deadline in group.deadline_set.all())
-        context = {
-            'groupName': str(group),
-            'userList': userList,
-            'deadlineList': deadlineList,
-        }
-    except Group.DoesNotExist:
-        raise Http404("Group does not exist")
+    group = get_object_or_404(Group, id=groupId)
+    userList = ', '.join(str(user) for user in group.user_set.all())
+    deadlineList = ', '.join(str(deadline) for deadline in group.deadline_set.all())
+    context = {
+        'groupName': str(group),
+        'userList': userList,
+        'deadlineList': deadlineList,
+    }
     return render(request, 'group.html', context)
 
 
 def userDetails(request, userId):
+    # TODO template
     user = User.objects.get(id=userId)
     return HttpResponse(f"this is user {user}. Soon we'll add more info"
                         f"{emojize(':thinking_face:')}")
