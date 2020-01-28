875d8cd39d5347e3db28087200899959cb541a74
DebDoDab <debbius3@gmail.com>
2020-01-28 21:59:07 +0300

    Some models added

--- Homework/models.py
+++ Homework/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Group(models.Model):
+    groupId = models.IntegerField()
+    groupToken = models.IntegerField()
+    name = models.CharField(max_length=10)
+
+
+class User(models.Model):
+    userId = models.IntegerField()
+    groupId = models.ForeignKey(Group, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=50)
+    passHash = models.CharField(max_length=128)
