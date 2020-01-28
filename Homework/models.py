64b5bb69141ef16bd3d9dffa96fb25d69e52278f
DebDoDab <debbius3@gmail.com>
2020-01-28 23:26:49 +0300

    Fixes

--- Homework/models.py
+++ Homework/models.py
@@ -1,16 +1,39 @@
 from django.db import models
-
+from django.utils.crypto import get_random_string
 # Create your models here.
 
 
 class Group(models.Model):
-    groupId = models.IntegerField()
-    groupToken = models.IntegerField()
-    name = models.CharField(max_length=10)
+    groupToken = models.CharField('token for inviting new users from that group', max_length=32)
+    name = models.CharField('name of a group (e.g. M3105)', max_length=10)
+
+    def __str__(self):
+        return self.name
+
+    def setToken(self):
+        token = get_random_string(32)
+        # TODO add check on same tokens
+        while False:
+            pass
+        self.groupToken = token
 
 
 class User(models.Model):
-    userId = models.IntegerField()
     groupId = models.ForeignKey(Group, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=50)
-    passHash = models.CharField(max_length=128)
+    nickname = models.CharField('just a nickname of a user', max_length=50)
+    passHash = models.CharField('hash for matching passwords', max_length=128)
+
+    def __str__(self):
+        return self.nickname
+
+    def registration(self, groupToken, nickname, password):
+        # TODO search group by token, find id
+        # groupId = ...
+        self.nickname = nickname  # TODO add check on same nicknames
+        self.passHash = password  # TODO add hashing
+
+    def login(self, nickname, password):
+        # TODO log in
+        # user = db.find(nickname)
+        #if password == user.passhash ...
+        pass
